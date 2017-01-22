61e0282b8c5e2fb4701c2f646a1760ee359c4401
Saori Matsumoto <garnerion@gmail.com>
2017-01-22 06:33:58 +0000

    mentor test

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,7 +29,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   
-  process resize_to_fill(100, 100, gravity = ::Magick::CenterGravity)
+  #process resize_to_fill(100, 100, gravity = ::Magick::CenterGravity)
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
